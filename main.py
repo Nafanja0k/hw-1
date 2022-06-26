0823a287d1531d914f1b402b2917b8b748f0b667
Oleksandr Diakin <odiakin@cisco.com>
2022-06-26 14:59:43 +0300

    done

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import argparse
 import hashlib
+import json
+import pandas as pd
+import csv
 import os
 import pwd
 import sys
+from datetime import datetime
 from enum import Enum
 
 import checksumdir
@@ -12,18 +16,29 @@ import dirhash as dirhash
 class Timecode(Enum):
     UNIX = 1
     UTC = 2
+
+
 class Path(Enum):
     SCRIPT_PATH = 0
     RUN_PATH = 1
     DEFINED_PATH = 2
+    def __str__(self):
+        return self.name
+
 
 class LS:
+    # default values
     script_path = os.path.abspath(os.path.dirname(__file__))
     run_path = os.getcwd()
     defined_path = ""
     timecode = Timecode.UNIX
+    location = Path.RUN_PATH
+
     def __init__(self, args):
         self.args = args
+
+        if args.location:
+            self.location = args.location
 
         if self.args.s:
             self.script_path = self.args.s
@@ -117,15 +132,27 @@ class LS:
         return dir_info
 
     def save_as_txt(self, path: str, content: str):
+        with open(path + "/output.txt", 'w') as file:
+            df = pd.DataFrame(content)
+            file.write(df.to_string(header=True, index=False))
         return
 
     def save_as_csv(self, path: str, content: str):
+        with open(path + "/output.csv", 'w') as file:
+            dict_writer = csv.DictWriter(file, content[0].keys())
+            dict_writer.writeheader()
+            dict_writer.writerows(content)
         return
 
     def save_as_json(self, path: str, content: str):
+        with open(path + "/output.json", 'w') as file:
+            json.dump(content, file, indent=4, sort_keys=True)
         return
 
     def save_as_html(self, path: str, content: str):
+        with open(path + "/output.html", 'w') as file:
+            df = pd.DataFrame(content)
+            file.write(df.to_html(header=True, index=False))
         return
 
     def get_task_status(self):
@@ -145,6 +172,7 @@ class LS:
         for directory in directories_list:
             full_path = "/".join([path, directory])
             data = self.get_directory_info(full_path, self.timecode)
+            data["type"] = "directory"
             data["name"] = directory
             data["path"] = path
             data["full_path"] = full_path
@@ -157,23 +185,50 @@ class LS:
         for file in files_list:
             full_path = "/".join([path, file])
             data = self.get_file_info(full_path, self.timecode)
+            data["type"] = "file"
             data["name"] = file
             data["path"] = path
             data["full_path"] = full_path
             files_info.append(data)
         return files_info
 
+    def get_contents_info(self, path: str):
+        contents_info = []
+        contents_info.extend(self.get_files_info(path))
+        contents_info.extend(self.get_directories_info(path))
+        return contents_info
 
+    def print_result(self, content, content_type="all", verbosity=0):
+        '''
+        prints result to stdout
+        :return:
+        '''
+
+        if self.verbosity:
+            print("Files in {} {}".format(str(self.location), ls.get_path(self.location)))
+            for item in content:
+                if self.timecode == Timecode.UTC:
+                    item["m_date"] = str(datetime.utcfromtimestamp(item["m_date"]))
+                    item["c_date"] = str(datetime.utcfromtimestamp(item["c_date"]))
+                print(json.dumps(item, indent=4))
+        else:
+            for item in content:
+                print(content)
+        return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('--help', action='help', help='show help message and exit')
+    parser.add_argument("--location", type=lambda path: Path[path], choices=list(Path), help="choose location to check files from")
+    parser.add_argument("--print", choices=["files", "directories", "all"], help="choose location to check files from")
     parser.add_argument("-f", action="store_true", help="only files")
     parser.add_argument("-d", action="store_true", help="only directories")
-    parser.add_argument("-t", action="store_true", help="output format - TXT")
-    parser.add_argument("-j", action="store_true", help="output format - JSON")
-    parser.add_argument("-c", action="store_true", help="output format - CSV")
-    parser.add_argument("-h", action="store_true", help="output format - HTML")
+
+    group_save_as = parser.add_mutually_exclusive_group()
+    group_save_as.add_argument("-t", action="store_true", help="output format - TXT")
+    group_save_as.add_argument("-j", action="store_true", help="output format - JSON")
+    group_save_as.add_argument("-c", action="store_true", help="output format - CSV")
+    group_save_as.add_argument("-h", action="store_true", help="output format - HTML")
 
     group_path = parser.add_mutually_exclusive_group()
     parser.add_argument("-r", help="path where the script is executed from")
@@ -191,21 +246,23 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     ls = LS(args)
+    if args.print:
+        if args.print == "all":
+            content = ls.get_contents_info(ls.get_path(ls.location))
+        elif args.print == "files":
+            content = ls.get_files_info(ls.get_path(ls.location))
+        elif args.print == "directories":
+            content = ls.get_files_info(ls.get_path(ls.location))
+        ls.print_result(content, verbosity=ls.verbosity)
 
-
-    content_run = ls.get_content_list(ls.get_path(Path.RUN_PATH))
-    print("Files in RUN_PATH {}".format(Path.RUN_PATH))
-    print(content_run)
-    content_script = ls.get_content_list(ls.get_path(Path.SCRIPT_PATH))
-    print("Files in SCRIPT_PATH {}".format(Path.RUN_PATH))
-    print(content_script)
-    content_defined = ls.get_content_list(ls.get_path(Path.DEFINED_PATH))
-    print("Files in DEFINED_PATH {}".format(ls.get_path(Path.DEFINED_PATH)))
-    print(content_defined)
-    print("Directories info in SCRIPT_PATH {}".format(ls.get_path(Path.DEFINED_PATH)))
-    print(ls.get_directories_info(ls.get_path(Path.RUN_PATH)))
-    print("Files info in SCRIPT_PATH {}".format(ls.get_path(Path.RUN_PATH)))
-    print(ls.get_files_info(ls.get_path(Path.RUN_PATH)))
-
+        #save to file if argument with output type is passed
+        if args.t:
+            ls.save_as_txt(path=ls.script_path, content=content)
+        elif args.j:
+            ls.save_as_json(path=ls.script_path, content=content)
+        elif args.c:
+            ls.save_as_csv(path=ls.script_path, content=content)
+        elif args.h:
+            ls.save_as_html(path=ls.script_path, content=content)
 
     sys.exit(0)
